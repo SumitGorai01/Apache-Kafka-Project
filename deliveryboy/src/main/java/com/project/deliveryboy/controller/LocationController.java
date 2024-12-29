717e7bd87ab31a18c39f405aedf675209ce5fd52
Sumit <goraisumit01@gmail.com>
2024-12-29 14:07:07 +0530

    first commit

---
+++ deliveryboy/src/main/java/com/project/deliveryboy/controller/LocationController.java
@@ -0,0 +1,33 @@
+package com.project.deliveryboy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.deliveryboy.service.KafkaService;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+
+	@Autowired
+	private KafkaService kafkaService;
+	
+	@PostMapping("/update")
+	public ResponseEntity<?> updateLocaiton(){
+		
+		for(int i=1;i<50;i++) {
+			
+			this.kafkaService.updateLocation("("+ Math.round(Math.random()*100)+" , " +Math.round(Math.random()*100)+")");
+			System.out.println("-------------------------------");
+
+		}
+		
+	
+		return new ResponseEntity<>(Map.of("message","Location updated"),HttpStatus.OK);
+	}
+}
